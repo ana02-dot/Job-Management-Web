734432fbd38bdc7bd19264e041575f6ba6c53d9e
ana02-dot <ana.chkhutiashvili.1@btu.edu.ge>
2025-12-16 17:08:24 +0400

    remove files from project

--- JobManagement-Back/JobManagement.Application/Interfaces/IPhoneVerificationService.cs
+++
@@ -1,8 +0,0 @@
-namespace JobManagement.Application.Interfaces;
-
-public interface IPhoneVerificationService
-{
-    Task<string> GenerateAndSendOtpAsync(string phoneNumber);
-    Task<bool> VerifyOtpAsync(string phoneNumber, string otpCode);
-}
-
