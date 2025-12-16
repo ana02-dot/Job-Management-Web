3d722e3cd9938cdf479763caa075616da7c27507
ana02-dot <ana.chkhutiashvili.1@btu.edu.ge>
2025-12-16 13:21:17 +0400

    remove user props

--- JobManagement-Back/JobManagement.Domain/Entities/User.cs
+++ JobManagement-Back/JobManagement.Domain/Entities/User.cs
@@ -6,15 +6,12 @@ namespace JobManagement.Domain.Entities;
 
 public class User : BaseEntity
 {
-    public string PersonalNumber { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
     public UserRole Role { get; set; }
-    public bool IsPersonalNumberVerified { get; set; }
-    public DateTime? PersonalNumberVerifiedAt { get; set; }
     public bool IsEmailVerified { get; set; }
     public DateTime? EmailVerifiedAt { get; set; }
 
