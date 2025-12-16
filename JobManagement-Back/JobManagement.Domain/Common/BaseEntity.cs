ac56b136f80b264af526f840a2a2811f0781fa97
ana02-dot <ana.chkhutiashvili.1@btu.edu.ge>
2025-12-16 13:15:59 +0400

    remove createdby and updatedby props

--- JobManagement-Back/JobManagement.Domain/Common/BaseEntity.cs
+++ JobManagement-Back/JobManagement.Domain/Common/BaseEntity.cs
@@ -7,8 +7,6 @@ public abstract class BaseEntity
     public int Id { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
-    public int? CreatedBy { get; set; }
-    public string? UpdatedBy { get; set; }
     public int IsDeleted { get; set; } = 0;
     public virtual User? Creator { get; set; }
 }
