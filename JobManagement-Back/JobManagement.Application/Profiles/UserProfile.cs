34c73acfed244cb2423b95d841d5503083b3aa50
ana02-dot <ana.chkhutiashvili.1@btu.edu.ge>
2025-12-16 17:12:06 +0400

    remove fields from userprofile

--- JobManagement-Back/JobManagement.Application/Profiles/UserProfile.cs
+++ JobManagement-Back/JobManagement.Application/Profiles/UserProfile.cs
@@ -11,9 +11,7 @@ public class UserProfile : Profile
         CreateMap<UserRegistrationRequest, User>()
             .ForMember(dest => dest.Id, opt => opt.Ignore())
             .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
-            .ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
             .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
-            .ForMember(dest => dest.UpdatedBy, opt => opt.Ignore())
             .ForMember(dest => dest.IsDeleted, opt => opt.Ignore())
             
             .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
