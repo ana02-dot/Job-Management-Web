fddd0bec92ab75cea4824e8a9aa6b205cbcbe6b1
ana02-dot <ana.chkhutiashvili.1@btu.edu.ge>
2025-12-16 17:10:49 +0400

    remove unused methods

--- JobManagement-Back/JobManagement.Application/Interfaces/IUserRepository.cs
+++ JobManagement-Back/JobManagement.Application/Interfaces/IUserRepository.cs
@@ -6,12 +6,10 @@ public interface IUserRepository
 {
     Task<User?> GetByIdAsync(int id);
     Task<User?> GetByEmailAsync(string email);
-    Task<User?> GetByPersonalNumberAsync(string personalNumber);
     Task<List<User>> GetAllAsync();
     Task<User> CreateAsync(User user);
     Task UpdateAsync(User user);
     Task DeleteAsync(int id);
     Task<bool> EmailExistsAsync(string email);
-    Task<bool> PersonalNumberExistsAsync(string personalNumber);
     Task<bool> PhoneNumberExistsAsync(string phoneNumber);
 }
