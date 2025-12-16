f0b64795ae845106c9110d7c86b0946d6b4b76a0
ana02-dot <ana.chkhutiashvili.1@btu.edu.ge>
2025-12-16 13:32:22 +0400

    add Job properties

--- JobManagement-Back/JobManagement.Domain/Entities/Job.cs
+++ JobManagement-Back/JobManagement.Domain/Entities/Job.cs
@@ -8,10 +8,13 @@ public class Job : BaseEntity
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string Requirements { get; set; } = string.Empty;
-    public decimal? Salary { get; set; }
+    public string Salary { get; set; } = string.Empty;
     public string Location { get; set; } = string.Empty;
     public JobStatus Status { get; set; } = JobStatus.Active;
     public DateTime ApplicationDeadline { get; set; }
+    public string WorkType { get; set; } = string.Empty; //remote, onsite, hybrid
+    public string Category { get; set; } = string.Empty;
+    public int? CreatedBy { get; set; }
     
     public virtual ICollection<Applications> Applications { get; set; } = new List<Applications>();
 }
