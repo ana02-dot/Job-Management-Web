ff603dc3b391f8d16e2d3b3f8263d86be7b7f988
ana02-dot <ana.chkhutiashvili.1@btu.edu.ge>
2026-01-08 01:18:32 +0400

    refactor code

--- JobManagement-Back/JobManagement.Infrastructure/Configuration/SwaggerConfiguration.cs
+++ JobManagement-Back/JobManagement.Infrastructure/Configuration/SwaggerConfiguration.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi;
 using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace JobManagement.Infrastructure.Configuration;
 
